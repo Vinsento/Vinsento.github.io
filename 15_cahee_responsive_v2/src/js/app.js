724ef6a7568beb919815d1103b59ffed5269da86
Vinsento <hummanist@gmail.com>
2019-10-30 00:21:20 +0200

    11+15 resp menu

---
+++ 15_cahee_responsive_v2/src/js/app.js
@@ -0,0 +1,5 @@
+document.querySelector('.nav__menu-btn').addEventListener('click', function(e){
+	e.preventDefault;
+	this.classList.toggle('active');
+	document.querySelector('.nav__list').classList.toggle('db');
+});
