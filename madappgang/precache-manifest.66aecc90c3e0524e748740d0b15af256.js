a27eac14e8770334308414fe721884e6d5dc9aa8
Vinsento <hummanist@gmail.com>
2019-12-20 22:31:15 +0200

    madappgang app uploded

---
+++ madappgang/precache-manifest.66aecc90c3e0524e748740d0b15af256.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf9c700f3f3c2844f1c58b1493b1c59",
+    "url": "/Vinsento/madappgang.git/index.html"
+  },
+  {
+    "revision": "290bc5ac3a1a1f8b661a",
+    "url": "/Vinsento/madappgang.git/static/css/main.08af0692.chunk.css"
+  },
+  {
+    "revision": "97bbc8fb380771c88a41",
+    "url": "/Vinsento/madappgang.git/static/js/2.727ec4ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Vinsento/madappgang.git/static/js/2.727ec4ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "290bc5ac3a1a1f8b661a",
+    "url": "/Vinsento/madappgang.git/static/js/main.3b46d7ff.chunk.js"
+  },
+  {
+    "revision": "a2d73a1df47be715c823",
+    "url": "/Vinsento/madappgang.git/static/js/runtime-main.4adf5e28.js"
+  },
+  {
+    "revision": "92770bfa7a12142ddf31f665f73565ac",
+    "url": "/Vinsento/madappgang.git/static/media/facebook.92770bfa.svg"
+  },
+  {
+    "revision": "1f4c1bad700e39a1c5c8d1e1f618e779",
+    "url": "/Vinsento/madappgang.git/static/media/instagram.1f4c1bad.svg"
+  },
+  {
+    "revision": "2d2b4b177c6945ddb1bcedcc3ec09aa2",
+    "url": "/Vinsento/madappgang.git/static/media/mailbox.2d2b4b17.svg"
+  },
+  {
+    "revision": "df5789fd2ef610c6ea5dc87aafbdca65",
+    "url": "/Vinsento/madappgang.git/static/media/main_bg.df5789fd.webp"
+  },
+  {
+    "revision": "d6f9dd03b677c134d64bdb68fb3a1736",
+    "url": "/Vinsento/madappgang.git/static/media/mc_book.d6f9dd03.svg"
+  },
+  {
+    "revision": "3643a37bf9739be224eea8b76952dda8",
+    "url": "/Vinsento/madappgang.git/static/media/medium.3643a37b.svg"
+  },
+  {
+    "revision": "f0c76512b47f2cdf38930ffd8682bffe",
+    "url": "/Vinsento/madappgang.git/static/media/twitter.f0c76512.svg"
+  }
+]);
